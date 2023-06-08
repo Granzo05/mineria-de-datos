603f75b595c6a07b70b3561dc60eda0670c5e5fc
granzo <facu.granzotto5@gmail.com>
2023-06-08 12:19:39 -0300

    Errores solucionados

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt5 import QtWidgets
-from Vistas.Menu import Menu
+
+from Vistas.LoginScreen import LoginScreen
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = Menu()
+    window = LoginScreen()
     window.show()
     sys.exit(app.exec_())
 
--- Vistas/LoginScreen.py
+++ Vistas/LoginScreen.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import random
-import sys
 import time
 
 from PyQt5 import uic, QtWidgets
-from PyQt5.QtCore import QCoreApplication
 from Database.EmpleadosDatabase import EmpleadosDatabase
 
 
@@ -20,6 +17,9 @@ class LoginScreen(QtWidgets.QMainWindow):
         self.salir.clicked.connect(self.salir_click)
 
         self.usuario_contra_inc.setVisible(False)
+
+        #Usuario: test
+        #Contraseña: test
 
     def buscar_usuario_login(self):
         nombre = self.usuarioJefe.text()
@@ -39,18 +39,36 @@ class LoginScreen(QtWidgets.QMainWindow):
 
     def abrir_menu(self):
         try:
-            app = QtWidgets.QApplication(sys.argv)
-            login_screen = LoginScreen()
-            login_screen.show()
-            sys.exit(app.exec_())
+            from Vistas.Menu import Menu
+            menu_screen = Menu()
+            menu_screen.show()
+            self.close()
         except Exception as e:
             print(f"Error al abrir la ventana: {str(e)}")
 
     def simulacion_carga(self):
         self.barraInicio.setVisible(True)
-        valor = 0  # Valor inicial del progreso
+        self.barraInicio.setMinimum(0)
+        self.barraInicio.setMaximum(100)
+        self.barraInicio.setTextVisible(False)
+
+        style_sheet = """
+            QProgressBar {
+                border: 2px solid grey;
+                border-radius: 5px;
+                background-color: #f0f0f0;
+                min-width: 275px;
+            }
+
+            QProgressBar::chunk {
+                background-color: #2196F3;
+                width: 20px;
+            }
+        """
+        self.barraInicio.setStyleSheet(style_sheet)
+
+        valor = 0
         while valor <= 100:
-            print("Progreso:", valor, "%")
 
             # Simular tiempo de espera
             time.sleep(0.01)
@@ -68,75 +86,4 @@ class LoginScreen(QtWidgets.QMainWindow):
 
             # Actualizar el progreso según la probabilidad de avance
             valor += avance
-=======
-import random
-import time
-
-from PyQt5 import uic, QtWidgets
-
-from Database.EmpleadosDatabase import EmpleadosDatabase
-from Vistas.AgregarEmpleado import AgregarEmpleado
-from Vistas.Menu import MainMenu
-
-
-class LoginScreen(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(LoginScreen, self).__init__()
-        uic.loadUi("Interfaz/login.ui", self)
-
-        # Conectar señales
-        self.barraInicio.setVisible(False)
-        self.iniciando.setVisible(False)
-        self.iniciaSesion.clicked.connect(self.buscar_usuario_login)
-        self.salir.clicked.connect(self.salir_click)
-
-        self.usuario_contra_inc.setVisible(False)
-
-    def buscar_usuario_login(self):
-        nombre = self.usuarioJefe.text()
-        contrasenia = self.contraJefe.text()
-
-        with EmpleadosDatabase() as empleados_data:
-            if empleados_data.buscar_usuario(nombre, contrasenia):
-                self.iniciando.setVisible(True)
-                self.simulacion_carga()
-                self.abrir_menu()
-            else:
-                self.usuario_contra_inc.setVisible(True)
-                print("Usuario o contraseña incorrectos")
-
-    def salir_click(self):
-        self.close()
-
-    def abrir_menu(self):
-        try:
-            self.hide()
-            window = MainMenu()
-            window.show()
-        except Exception as e:
-            print(f"Error al abrir la ventana: {str(e)}")
-
-    def simulacion_carga(self):
-        self.barraInicio.setVisible(True)
-        valor = 0  # Valor inicial del progreso
-        while valor <= 100:
-            print("Progreso:", valor, "%")
-
-            # Simular tiempo de espera
-            time.sleep(0.01)
-
-            probabilidad = random.randint(1, 100)
-
-            if probabilidad <= 45:
-                avance = 2
-            elif probabilidad <= 30:
-                avance = 3
-            elif probabilidad <= 15:
-                avance = 4
-            else:
-                avance = 1
-
-            # Actualizar el progreso según la probabilidad de avance
-            valor += avance
->>>>>>> 678ac8ad9da9210174037461053acf57dc6588f3
             self.barraInicio.setValue(valor)
