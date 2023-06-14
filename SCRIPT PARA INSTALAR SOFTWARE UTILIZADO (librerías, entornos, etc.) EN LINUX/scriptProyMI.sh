3094374eddb73e230446213a285429fd9baa9c2d
Augusto Ficara <134169439+ADJF2003@users.noreply.github.com>
2023-06-14 15:03:30 -0300

    Add files via upload

---
+++ SCRIPT PARA INSTALAR SOFTWARE UTILIZADO (librerías, entornos, etc.) EN LINUX/scriptProyMI.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9 -y
+python3.9 --version
+sudo apt install snapd
+sudo snap install pycharm-community --classic
+sudo apt install python3-pip
+pip install pyqt5
+pip install pyside2
+pip install numpy
+pip install schedule
+sudo apt install sqlite3
+sudo apt-get install python3-pyqt5
+pip install pyqt5-sip
+pip install pyqt5-tools
+sudo apt install qtchooser
+pip3 install PyQtWebEngine
+sudo apt-get install qttools5-dev-tools
+pip3 install --user -I PyQt5
+pip3 install --user -I PyQt5-sip
+sudo apt install python3.x
+pip3 install pyqt5
+pip install pandas
+pip install matplotlib
