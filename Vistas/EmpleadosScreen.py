828177570cbf23c25126f8ecfa8ed2ac43f18d2b
granzo <facu.granzotto5@gmail.com>
2023-06-22 10:39:20 -0300

    ""

--- Vistas/EmpleadosScreen.py
+++ Vistas/EmpleadosScreen.py
@@ -14,8 +14,8 @@ class EmpleadosScreen(QtWidgets.QMainWindow):
         self.volver.clicked.connect(lambda: self.volver_screen())
         self.salir.clicked.connect(self.salir_screen)
         self.cerrarSesion.clicked.connect(self.cerrar_sesion)
-        self.ultimos31.clicked.connect(lambda: self.mostrar_empleados_por_parametros_graficos(dias=31))
-        self.ultimos14.clicked.connect(lambda: self.mostrar_empleados_por_parametros_graficos(dias=14))
+        self.ultimos31.clicked.connect(lambda: self.mostrar_empleados_por_parametros_graficos(dias=31, id=self.idRendimiento.text()))
+        self.ultimos14.clicked.connect(lambda: self.mostrar_empleados_por_parametros_graficos(dias=14, id=self.idRendimiento.text()))
 
         self.errorIdFecha.setVisible(False)
         self.buscarApellido.setVisible(False)
@@ -55,12 +55,6 @@ class EmpleadosScreen(QtWidgets.QMainWindow):
         self.buscarApellido.textChanged.connect(self.actualizar_empleados_por_parametros)
         self.buscarCargo.textChanged.connect(self.actualizar_empleados_por_parametros)
         self.buscarTurno.textChanged.connect(self.actualizar_empleados_por_parametros)
-
-        # Campos para crear grafico
-        self.diasGrafico.textChanged.connect(self.actualizar_empleados_por_parametros_graficos)
-        self.turnoGrafico.textChanged.connect(self.actualizar_empleados_por_parametros_graficos)
-        self.cargoGrafico.textChanged.connect(self.actualizar_empleados_por_parametros_graficos)
-        self.idGrafico.textChanged.connect(self.actualizar_empleados_por_parametros_graficos)
 
         fecha_inicio = QDate(2023, 4, 27)
 
@@ -147,7 +141,7 @@ class EmpleadosScreen(QtWidgets.QMainWindow):
         if fechaInput and id:
             with EmpleadosDatabase() as empleados_data:
                 empleados_data.buscar_rendimiento_empleado(id, fechaInput, self.tablaDatos)
-
+                empleados_data.filtrar_por_campos_empleado(id, self.tablaEmpleados)
             self.errorIdFecha.setVisible(False)
         else:
             self.errorIdFecha.setVisible(True)
@@ -191,95 +185,138 @@ class EmpleadosScreen(QtWidgets.QMainWindow):
         except Exception as e:
             print("Error al obtener los empleados:", str(e))
 
-    def actualizar_empleados_por_parametros_graficos(self):
+    def actualizar_empleados_por_parametros_graficos(self, id):
         dias = self.diasGrafico.text()
-        turno = self.turnoGrafico.text()
-        cargo = self.cargoGrafico.text()
-        id = self.idGrafico.text()
-
-        self.busquedaPersonalizada.clicked.connect(
-            lambda: self.mostrar_empleados_por_parametros_graficos(dias, id, cargo, turno))
-        self.errorIdFecha.setVisible(False)
-
-    def mostrar_empleados_por_parametros_graficos(self, dias=None, id=None, cargo=None, turno=None):
         print(dias)
-        print(turno)
-        print(cargo)
         print(id)
-        try:
+        if dias and id:
+            self.busquedaPersonalizada.clicked.connect(
+                lambda: self.mostrar_empleados_por_parametros_graficos(dias, id))
+            self.errorIdFecha.setVisible(False)
+        else:
+            "Todo: Mostrar cartel de que falta el dia o el id"
+
+    def mostrar_empleados_por_parametros_graficos(self, dias=None, id=None):
+        if dias and id:
+            try:
+                with EmpleadosDatabase() as empleados_data:
+                    query = "SELECT * FROM empleados WHERE 1=1"
+                    params = []
+
+                    if id:
+                        query += " AND id LIKE ?"
+                        params.append(f"%{id}%")
+
+                    # Obtener los IDs de los empleados que coinciden con los filtros
+                    ids_empleados = empleados_data.filtrar_por_campos_grafico(query, params)
+
+                    with EmpleadosDatabase() as empleados_data:
+                        resultados = empleados_data.filtrar_por_campos_para_graficar(dias, ids_empleados)
+
+                    # Convertir la lista de empleados en un DataFrame de pandas
+                    df = pd.DataFrame(resultados,
+                                      columns=['fecha', 'pasos_realizados', 'horas_de_trabajo', 'asistencia',
+                                               'nivel_estres',
+                                               'empleado_id'])
+
+                    df['fecha'] = pd.to_datetime(df['fecha'])
+
+                    filtro_dias = df[df['fecha'] >= pd.Timestamp.now() - pd.DateOffset(days=dias)]
+                    print(filtro_dias)
+                    # Imprimir los resultados de cada día
+                    for _, row in filtro_dias.iterrows():
+                        fecha = row['fecha'].strftime("%d-%m-%Y")
+                        pasos_realizados = row['pasos_realizados']
+                        horas_de_trabajo = row['horas_de_trabajo']
+                        asistencia = row['asistencia']
+                        nivel_estres = row['nivel_estres']
+                        empleado_id = row['empleado_id']
+
+                        print("Fecha:", fecha)
+                        print("Pasos realizados:", pasos_realizados)
+                        print("Horas de trabajo:", horas_de_trabajo)
+                        print("Asistencia:", asistencia)
+                        print("Nivel de estrés:", nivel_estres)
+                        print("ID del empleado:", empleado_id)
+                        print("--------------------------------------")
+
+
+                    # Crear gráfico para los pasos
+                    plt.subplot(2, 1, 1)
+                    plt.plot(pasos_realizados, pasos_realizados, label='Pasos Realizados')
+                    plt.xlabel('Fecha')
+                    plt.ylabel('Pasos')
+                    plt.title('Pasos Realizados en los últimos {} días'.format(dias))
+                    plt.xticks(rotation=45)
+                    plt.legend()
+
+                    # Crear gráfico combinado para el estrés, asistencia y horas
+                    plt.subplot(2, 1, 2)
+                    plt.plot(horas_de_trabajo, horas_de_trabajo, label='Horas de Trabajo')
+                    plt.plot(nivel_estres, nivel_estres, label='Nivel de Estrés')
+                    plt.xlabel('Fecha')
+                    plt.ylabel('Valor')
+                    plt.title('Horas de Trabajo, Asistencia y Nivel de Estrés en los últimos {} días'.format(dias))
+                    plt.xticks(rotation=45)
+                    plt.legend()
+
+                    # Mostrar ambos gráficos
+                    plt.tight_layout()
+                    plt.show()
+
+            except Exception as e:
+                print("Error al obtener los empleados:", str(e))
+        else:
+            try:
+                with EmpleadosDatabase() as empleados_data:
+                    query = "SELECT * FROM empleados"
+                    params = []
+
+                    # Obtener los IDs de los empleados que coinciden con los filtros
+                    ids_empleados = empleados_data.filtrar_por_campos_grafico(query, params)
+
+            except Exception as e:
+                print("Error al obtener los empleados:", str(e))
+
             with EmpleadosDatabase() as empleados_data:
-                query = "SELECT * FROM empleados WHERE 1=1"
-                params = []
+                resultados = empleados_data.filtrar_por_campos_para_graficar(dias, ids_empleados)
 
-                if turno:
-                    query += " AND turno LIKE ?"
-                    params.append(f"%{turno}%")
+            # Convertir la lista de empleados en un DataFrame de pandas
+            df = pd.DataFrame(resultados,
+                              columns=['fecha', 'pasos_realizados', 'horas_de_trabajo', 'asistencia', 'nivel_estres',
+                                       'empleado_id'])
 
-                if cargo:
-                    query += " AND cargo LIKE ?"
-                    params.append(f"%{cargo}%")
+            df['fecha'] = pd.to_datetime(df['fecha'])
 
-                if id:
-                    query += " AND id LIKE ?"
-                    params.append(f"%{id}%")
+            filtro_dias = df[df['fecha'] >= pd.Timestamp.now() - pd.DateOffset(days=dias)]
 
-                if id and cargo:
-                    query += " AND (id LIKE ? AND cargo LIKE ?)"
-                    params.extend([f"%{id}%", f"%{cargo}%"])
+            # Calcular las sumas de los parámetros para todos los empleados en un día
+            suma_pasos = filtro_dias.groupby('fecha')['pasos_realizados'].sum()
+            suma_horas_trabajo = filtro_dias.groupby('fecha')['horas_de_trabajo'].sum()
+            suma_asistencia = filtro_dias.groupby('fecha')['asistencia'].apply(
+                lambda x: (x == 'Presente').sum())
+            suma_estres = filtro_dias.groupby('fecha')['nivel_estres'].mean()
 
-                if id and turno:
-                    query += " AND (id LIKE ? AND turno LIKE ?)"
-                    params.extend([f"%{id}%", f"%{turno}%"])
+            # Crear gráfico para los pasos
+            plt.subplot(2, 1, 1)
+            plt.plot(suma_pasos.index, suma_pasos, label='Pasos Realizados')
+            plt.xlabel('Fecha')
+            plt.ylabel('Pasos')
+            plt.title('Pasos Realizados en los últimos {} días'.format(dias))
+            plt.xticks(rotation=45)
+            plt.legend()
 
-                if cargo and turno:
-                    query += " AND (cargo LIKE ? AND turno LIKE ?)"
-                    params.extend([f"%{cargo}%", f"%{turno}%"])
+            # Crear gráfico combinado para el estrés, asistencia y horas
+            plt.subplot(2, 1, 2)
+            plt.plot(suma_horas_trabajo.index, suma_horas_trabajo, label='Horas de Trabajo')
+            plt.plot(suma_asistencia.index, suma_asistencia, label='Asistencia')
+            plt.plot(suma_estres.index, suma_estres, label='Nivel de Estrés')
+            plt.xlabel('Fecha')
+            plt.ylabel('Valor')
+            plt.title('Horas de Trabajo, Asistencia y Nivel de Estrés en los últimos {} días'.format(dias))
+            plt.xticks(rotation=45)
+            plt.legend()
 
-                # Obtener los IDs de los empleados que coinciden con los filtros
-                ids_empleados = empleados_data.filtrar_por_campos_grafico(query, params)
-
-        except Exception as e:
-            print("Error al obtener los empleados:", str(e))
-
-        with EmpleadosDatabase() as empleados_data:
-            resultados = empleados_data.filtrar_por_campos_para_graficar(dias, ids_empleados)
-
-        # Convertir la lista de empleados en un DataFrame de pandas
-        df = pd.DataFrame(resultados,
-                          columns=['fecha', 'pasos_realizados', 'horas_de_trabajo', 'asistencia', 'nivel_estres',
-                                   'empleado_id'])
-
-        df['fecha'] = pd.to_datetime(df['fecha'])
-
-        filtro_dias = df[df['fecha'] >= pd.Timestamp.now() - pd.DateOffset(days=dias)]
-
-        # Calcular las sumas de los parámetros para todos los empleados en un día
-        suma_pasos = filtro_dias.groupby('fecha')['pasos_realizados'].sum()
-        suma_horas_trabajo = filtro_dias.groupby('fecha')['horas_de_trabajo'].sum()
-        suma_asistencia = filtro_dias.groupby('fecha')['asistencia'].apply(
-            lambda x: (x == 'Presente').sum())
-        suma_estres = filtro_dias.groupby('fecha')['nivel_estres'].mean()
-
-        # Crear gráfico para los pasos
-        plt.subplot(2, 1, 1)
-        plt.plot(suma_pasos.index, suma_pasos, label='Pasos Realizados')
-        plt.xlabel('Fecha')
-        plt.ylabel('Pasos')
-        plt.title('Pasos Realizados en los últimos {} días'.format(dias))
-        plt.xticks(rotation=45)
-        plt.legend()
-
-        # Crear gráfico combinado para el estrés, asistencia y horas
-        plt.subplot(2, 1, 2)
-        plt.plot(suma_horas_trabajo.index, suma_horas_trabajo, label='Horas de Trabajo')
-        plt.plot(suma_asistencia.index, suma_asistencia, label='Asistencia')
-        plt.plot(suma_estres.index, suma_estres, label='Nivel de Estrés')
-        plt.xlabel('Fecha')
-        plt.ylabel('Valor')
-        plt.title('Horas de Trabajo, Asistencia y Nivel de Estrés en los últimos {} días'.format(dias))
-        plt.xticks(rotation=45)
-        plt.legend()
-
-        # Mostrar ambos gráficos
-        plt.tight_layout()
-        plt.show()
+            # Mostrar ambos gráficos
+            plt.tight_layout()
+            plt.show()
